e84ca3d15c7b2860915808e9938a48351449b170
Mather <misaka@misaka.im>
2017-03-12 22:38:59 +0800

    + HTML Webpack Plugin.

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,21 @@
 /**
  * Created by Administrator on 2017-03-11.
  */
+
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     entry: './src/app.js',
     output: {
-        filename: './dist/app.bundle.js'
-    }
+        path: 'dist',
+        filename: 'app.bundle.js'
+    },
+    plugins: [new HtmlWebpackPlugin({
+        title: 'Project Demo',
+        minify: {
+            collapseWhitespace: true
+        },
+        hash:true,
+        template: './src/index.html', // Load a custom template (ejs by default see the FAQ for details)
+    })]
 
-}
+};
