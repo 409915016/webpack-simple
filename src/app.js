1633ac1c9410c9324321a6c8854607d79658b4bd
Mather <409915016@qq.com>
2017-03-15 16:50:18 +0800

    + Webpack Dev Server Configuration.

--- src/app.js
+++ src/app.js
@@ -1,2 +1,2 @@
 const css = require('./app.less');
-console.log("hello!!! form app.js and webpack dev server!");
+console.log("hello!!! form app.js and webpack dev server!!");
--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,13 @@ module.exports = {
             }
         ]
     },
+    devServer: {
+        contentBase: path.join(__dirname, "dist"),
+        compress: true,
+        port: 9000,
+        stats: "errors-only",
+        open: true
+    },
     plugins: [
         new HtmlWebpackPlugin({
             title: 'Project Demo',
